9041408d27585c2cc9b7ea2f69653e1be0493c60
janny801 <jred8069@gmail.com>
2026-02-06 17:58:49 -0600

    function to push into the priority queue

--- manual/priorityqueue.py
+++ manual/priorityqueue.py
@@ -1,3 +1,19 @@
 #practice making a priority queue using a minheap 
 #no 'import heapq' 
 
+class priorityqueue: 
+    def __init__(self):  
+        self.heap=[]
+    
+    def push(self, item): 
+        #add item to the end of the list first then do the reordering
+            #this preserves complete binary tree structure 
+            #heap shape is correct, but minheap ordering might be incorrect
+        self.heap.append(item) 
+
+
+
+
+
+
+
